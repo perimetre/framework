f00c3cba3fab692ab3d1cc4534c652c58e917fa0
AssisrMatheus <assisr.matheus@gmail.com>
2025-12-02 08:14:30 -0500

    fix: cli breaking

--- packages/service-builder/eslint.config.mjs
+++ packages/service-builder/eslint.config.mjs
@@ -4,10 +4,19 @@ import endConfig from '@perimetre/eslint-config-base/end';
 export default [
   ...baseConfig,
   {
+    files: ['src/**/*', '**/*.test.ts'],
     languageOptions: {
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  {
+    files: ['tsdown.config.ts'],
+    languageOptions: {
+      parserOptions: {
+        allowDefaultProject: true
       }
     }
   },
