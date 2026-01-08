a20356746a7bc0abeeaff4525213f2b8026df71e
AssisrMatheus <assisr.matheus@gmail.com>
2026-01-08 14:22:14 -0500

    fix(ui): fixes flex container

---
+++ .changeset/frank-ideas-doubt.md
@@ -0,0 +1,5 @@
+---
+'@perimetre/ui': patch
+---
+
+Fix image wrapper flex container
--- packages/ui/src/components/ImageCarousel/ImageCarouselLazyLoad/brands/ImageCarouselImageWrapper.acorn.brand.ts
+++ packages/ui/src/components/ImageCarousel/ImageCarouselLazyLoad/brands/ImageCarouselImageWrapper.acorn.brand.ts
@@ -4,7 +4,14 @@ import { cva } from '@/lib/cva';
  * Acorn brand ImageCarouselImageWrapper variant
  */
 export const imageCarouselImageWrapperAcornVariants = cva({
-  base: ['pui:transition-opacity', 'pui:duration-200', 'pui:h-full'],
+  base: [
+    'pui:transition-opacity',
+    'pui:duration-200',
+    'pui:h-full',
+    'pui:flex',
+    'pui:items-center',
+    'pui:justify-center'
+  ],
   variants: {
     loaded: {
       false: 'pui:opacity-0',
