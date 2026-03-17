d331f14828254472d254e64530ca4a542243f3cc
Charline Mons <48532888+charlinemons@users.noreply.github.com>
2026-03-17 15:12:42 -0400

    feat: add data-pui-component prop to all Image Carousel sub-component

--- packages/ui/src/components/ImageCarousel/ImageCarouselLazyLoad/brands/index.ts
+++ packages/ui/src/components/ImageCarousel/ImageCarouselLazyLoad/brands/index.ts
@@ -1,14 +1,16 @@
 import { type BrandVariants } from '@/lib/brand-registry';
+import { compose } from '@/lib/cva';
 import { imageCarouselImageWrapperAcornVariants } from './ImageCarouselImageWrapper.acorn.brand';
 import { imageCarouselLazyLoadContainerAcornVariants } from './ImageCarouselLazyLoadContainer.acorn.brand';
+import { imageCarouselLazyLoadContainerSprigVariants } from './ImageCarouselLazyLoadContainer.sprig.brand';
 import { imageCarouselLazyLoadSpinnerAcornVariants } from './ImageCarouselLazyLoadSpinner.acorn.brand';
 
 export const imageCarouselLazyLoadContainerBrandVariants = {
-  acorn: imageCarouselLazyLoadContainerAcornVariants
-  // sprig: compose(
-  //   imageCarouselLazyLoadContainerAcornVariants,
-  //   imageCarouselLazyLoadContainerSprigVariants
-  // ),
+  acorn: imageCarouselLazyLoadContainerAcornVariants,
+  sprig: compose(
+    imageCarouselLazyLoadContainerAcornVariants,
+    imageCarouselLazyLoadContainerSprigVariants
+  )
   // stelpro: compose(
   //   imageCarouselLazyLoadContainerAcornVariants,
   //   imageCarouselLazyLoadContainerStelproVariants
