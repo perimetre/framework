a3ab7d8702a0fd3ac2d0743e030385a225df5f08
miasalav <miasalaveria@gmail.com>
2026-04-14 10:37:53 -0400

    Blocks/section horizontal header (#85)
    * feat: create section horizontal header component
    
    * chore: setting up horizontal header
    
    setting up horizontal header
    
    * chore: adding variants and fonts for section horizontal header
    
    added fonts for microbird, added variants for button styling, added variants for header styling
    
    * chore: microbird commercial css update
    
    * chore: lint warning fix
    
    * chore: qa fixes from matheus
    
    * chore: fixing typecheck error
    
    * chore: qa fixes continued
    
    fixed class inheritance, removed redundant code,   implemented 'as' prop in the title
    
    * chore: fixing type error and removing h1 variant
    
    * chore: created changeset
    
    * chore: updating tag inside eyebrow field
    
    * feat: adding mobile styles to sectionhorizontalheader
    
    * feat: added updated changeset
    
    * feat: removing alignmobile and adding cn
    
    ---------
    
    Co-authored-by: Matheus Assis <assisr.matheus@gmail.com>

---
+++ .changeset/tricky-parks-sit.md
@@ -0,0 +1,6 @@
+---
+'@perimetre/tokens': minor
+'@perimetre/ui': minor
+---
+
+Created SectionHorizontalHeader component
--- packages/ui/src/components/SectionHorizontalHeader/brands/SectionHorizontalHeader.acorn.brand.ts
+++ packages/ui/src/components/SectionHorizontalHeader/brands/SectionHorizontalHeader.acorn.brand.ts
@@ -4,7 +4,7 @@ import { cva } from '@/lib/cva';
  * Acorn brand SectionHorizontalHeader variants (default/base theme)
  */
 export const sectionHorizontalHeaderAcornVariants = cva({
-  base: ['pui:font-pui-sans pui:space-y-5'],
+  base: ['pui:font-pui-sans pui:space-y-5 pui:text-center pui:lg:text-left'],
   variants: {
     variant: {
       default: [],
@@ -15,11 +15,11 @@ export const sectionHorizontalHeaderAcornVariants = cva({
 });
 
 export const sectionHorizontalHeaderTitleAcornVariants = cva({
-  base: ['pui:font-pui-display'],
+  base: ['pui:font-pui-display pui:uppercase'],
   variants: {
     variant: {
-      default: ['pui:typo-heading-1'],
-      compact: ['pui:typo-heading-2']
+      default: ['pui:text-3xl pui:lg:typo-heading-1'],
+      compact: ['pui:text-4xl pui:lg:typo-heading-2']
     }
   },
   defaultVariants: { variant: 'default' }
