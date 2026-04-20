d9b97143cf83b1e190e9ef173966213c76a97a84
miasalav <miasalaveria@gmail.com>
2026-04-20 12:22:31 -0400

    feat: adding new button styles

--- packages/ui/src/components/Button/Button.stories.tsx
+++ packages/ui/src/components/Button/Button.stories.tsx
@@ -16,6 +16,11 @@ export default {
       options: ['small', 'default'],
       control: { type: 'select' },
       defaultValue: 'default'
+    },
+    variant: {
+      options: ['primary', 'outline', 'primaryLight', 'transparent'],
+      control: { type: 'select' },
+      defaultValue: 'primary'
     }
   }
 } satisfies StoryDefault<Props>;
@@ -26,8 +31,56 @@ const DefaultComp: Story<Props> = ({ label, ...props }) => (
 
 export const Default = DefaultComp.bind({});
 
+export const Primary = DefaultComp.bind({});
+Primary.args = {
+  label: 'Shop Now',
+  variant: 'primary'
+};
+
+export const Outline = DefaultComp.bind({});
+Outline.args = {
+  label: 'Learn More',
+  variant: 'outline'
+};
+
+export const PrimaryLight = DefaultComp.bind({});
+PrimaryLight.args = {
+  label: 'View Details',
+  variant: 'primaryLight'
+};
+
+export const Transparent = DefaultComp.bind({});
+Transparent.args = {
+  label: 'Cancel',
+  variant: 'transparent'
+};
+
 export const Small = Default.bind({});
 Small.args = {
   label: 'Shop',
   size: 'small'
 };
+
+export const AllVariants: Story<Props> = ({ label = 'Shop Now', ...props }) => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      gap: '1rem',
+      alignItems: 'flex-start'
+    }}
+  >
+    <Button {...props} variant="primary">
+      {label}
+    </Button>
+    <Button {...props} variant="outline">
+      {label}
+    </Button>
+    <Button {...props} variant="primaryLight">
+      {label}
+    </Button>
+    <Button {...props} variant="transparent">
+      {label}
+    </Button>
+  </div>
+);
