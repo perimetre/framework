acfab6096bc59fea515c5bb4859888a465910a8e
miasalav <miasalaveria@gmail.com>
2026-04-22 11:31:17 -0400

    Fixes/section horizontal header styling (#107)
    * feat: fixing mobile text sizes
    
    * feat: changing breakpoint to md instead of lg
    
    * feat: adding changeset

---
+++ .changeset/wet-mails-peel.md
@@ -0,0 +1,5 @@
+---
+'@perimetre/ui': minor
+---
+
+Fixing mobile styles for section horizontal header
--- packages/ui/src/components/SectionHorizontalHeader/brands/SectionHorizontalHeader.acorn.brand.ts
+++ packages/ui/src/components/SectionHorizontalHeader/brands/SectionHorizontalHeader.acorn.brand.ts
@@ -4,7 +4,7 @@ import { cva } from '@/lib/cva';
  * Acorn brand SectionHorizontalHeader variants (default/base theme)
  */
 export const sectionHorizontalHeaderAcornVariants = cva({
-  base: ['pui:font-pui-sans pui:space-y-5 pui:text-center pui:lg:text-left'],
+  base: ['pui:font-pui-sans pui:space-y-5 pui:text-center pui:md:text-left'],
   variants: {
     variant: {
       default: [],
@@ -15,11 +15,11 @@ export const sectionHorizontalHeaderAcornVariants = cva({
 });
 
 export const sectionHorizontalHeaderTitleAcornVariants = cva({
-  base: ['pui:font-pui-display pui:uppercase'],
+  base: ['pui:font-pui-display pui:uppercase pui:mb-2'],
   variants: {
     variant: {
-      default: ['pui:text-3xl pui:lg:typo-heading-1'],
-      compact: ['pui:text-4xl pui:lg:typo-heading-2']
+      default: ['pui:text-[2.5rem] pui:md:typo-heading-1'],
+      compact: ['pui:text-[2.125rem] pui:md:typo-heading-2']
     }
   },
   defaultVariants: { variant: 'default' }
